d4541f5aa6061faa019e1984cfb4b6f788d27353
lcg0826 <changyu0826@naver.com>
2021-09-30 01:05:21 +0900

    Mustache 사용하여 메인 페이지 만들고 생성 조회 테스트 - index.mustache - index.js - header.mustache - footer.mustache - posts-save.mustache - indexController.java  수정

--- src/main/java/com/bootAwsBooks/prac/web/indexController.java
+++ src/main/java/com/bootAwsBooks/prac/web/indexController.java
@@ -2,6 +2,7 @@ package com.bootAwsBooks.prac.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class indexController {
@@ -9,5 +10,10 @@ public class indexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
