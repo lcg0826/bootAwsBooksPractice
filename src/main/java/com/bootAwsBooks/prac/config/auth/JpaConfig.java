814e0ea2a1c38a6a6c61caef23e54741fd47838c
lcg0826 <changyu0826@naver.com>
2021-10-07 01:06:36 +0900

    기존 테스트에 시큐리티 미적용 오류 부분 수정중

---
+++ src/main/java/com/bootAwsBooks/prac/config/auth/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.bootAwsBooks.prac.config.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // JPA Auditing 활성화
+public class JpaConfig {}
